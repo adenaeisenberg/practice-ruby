c18009f2390cfc58c84997b02f0470b89bb4ad82
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-06-27 15:27:18 -0400

    puts apples

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
 apples = { :color => "red", :price => 1, :inventory => 250 }
 bananas = { color: "yellow", price: 2, inventory: "120" }
 bread = { color: "brown", price: 5, inventory: 60 }
+
+puts "Our store sells #{apples[:color]} fruit for #{apples[:price]} dollars amd we have #{apples[:inventory]} in stock"
