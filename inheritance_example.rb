ea696c31c1ee42a293aa75ac55e9f23c851cf780
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-06-28 14:44:58 -0400

    more edits

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,8 +1,11 @@
-class Car
+class Transportation
   def initialize
     @speed = 0
     @direction = "north"
   end
+
+  attr_reader :speed, :direction
+  attr_writer :speed, :direction
 
   def brake
     @speed = 0
@@ -15,31 +18,22 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+class Car < Transportation
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = "north"
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Transportation
   def ring_bell
     puts "Ring ring!"
   end
 end
+
+my_car = Car.new({})
+my_bike = Bike.new
+
+my_car.accelerate
+my_bike.accelerate
