09fd27fd89590ecb213546012369772935937903
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-06-28 14:06:47 -0400

    added hashes

--- store_item.rb
+++ store_item.rb
@@ -23,10 +23,10 @@ class Store
   end
 end
 
-apples = Store.new(input_color: "red", input_inventory: 250, input_price: 1)
-bananas = Store.new(input_color: "yellow", input_price: 2, input_inventory: 120)
-bread = Store.new(input_price: 5, input_inventory: 60, input_color: "brown")
-oranges = Store.new(input_color: "orange", input_price: 0.75, input_inventory: 45)
+apples = Store.new({ input_color: "red", input_inventory: 250, input_price: 1 })
+bananas = Store.new({ input_color: "yellow", input_price: 2, input_inventory: 120 })
+bread = Store.new({ input_price: 5, input_inventory: 60, input_color: "brown" })
+oranges = Store.new({ input_color: "orange", input_price: 0.75, input_inventory: 45 })
 
 apples.item_info
 bananas.item_info
