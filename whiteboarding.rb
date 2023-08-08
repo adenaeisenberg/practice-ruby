c49219ea1fdb8299244a313c8bb0b7a750db0663
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-08 16:44:12 -0400

    whiteboarding

---
+++ whiteboarding.rb
@@ -0,0 +1,24 @@
+# 2) Write a function that takes in an array of strings and returns the smallest string.
+
+# def shortest_words(strings)
+#   index = 0
+#   index_2 = 1
+#   shortest_word = ""
+#   while index_2 < strings.length
+#     if strings[index].length < strings[index_2].length
+#       shortest_word = strings[index]
+#     end
+#     index = index + 1
+#     index_2 = index_2 + 1
+#   end
+# end
+
+# p shortest_words(["longest", "short", "uh", "a"])
+
+# 3) Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+
+# def reverse_numbers(numbers)
+#   return numbers.reverse
+# end
+
+# p reverse_numbers([1, 2, 3, 4, 5])
