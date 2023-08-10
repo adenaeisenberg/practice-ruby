b1bf2bee2bb002ab51ee8083a9ee94e2afe46ad8
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-10 17:03:46 -0400

    whiteboarding for today

--- whiteboarding.rb
+++ whiteboarding.rb
@@ -59,5 +59,26 @@
 #   7) Write a function that takes in an array of numbers and returns the two smallest numbers.
 
 #   8) Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+
 #   9) Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+
+# input-- [2, 6, 34, 98]
+# output-- false
+
+# input- [11, 20, 30, 40]
+# output- true
+
+# - make a function that accepts an array
+# -create an index
+# - create a while loop that itterates thru the array and checks at each index whether the index is greater than 10
+# - if the number is less than 10 << new_array
+# - increment index by 1
+# - end
+
+def greater_than_ten(array)
+end
+
+p greater_than_ten([2, 6, 34, 98])
+# try this one too p greater_than_ten([11, 20, 30, 40])
+
 #   10) Write a function that takes in an array of words and returns the number of times the letter “a” appeared in total.
